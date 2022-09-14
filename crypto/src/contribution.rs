45bc9b13cf36434da4ec11751c987fa030bffb17
Remco Bloemen <remco@wicked.ventures>
2022-09-14 23:44:05 +0200

    Add crypto lib

---
+++ crypto/src/contribution.rs
@@ -0,0 +1,409 @@
+use crate::{crypto::g1_mul_glv, g1_subgroup_check, g2_subgroup_check, parse_g, ParseError};
+use ark_bls12_381::{g1, g2, Bls12_381, Fr, G1Affine, G1Projective, G2Affine, G2Projective};
+use ark_ec::{msm::VariableBaseMSM, AffineCurve, PairingEngine, ProjectiveCurve};
+use ark_ff::{One, PrimeField, UniformRand, Zero};
+use rayon::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::{cmp::max, iter};
+use thiserror::Error;
+use tracing::{error, instrument};
+use zeroize::Zeroizing;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Transcript {
+    pub g1_powers: Vec<G1Affine>,
+    pub g2_powers: Vec<G2Affine>,
+    pub products: Vec<G1Affine>,
+    pub pubkeys: Vec<G2Affine>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Contribution {
+    pub pubkey: G2Affine,
+    pub g1_powers: Vec<G1Affine>,
+    pub g2_powers: Vec<G2Affine>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContributionsJson {
+    pub sub_contributions: Vec<ContributionJson>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContributionJson {
+    pub num_g1_powers: usize,
+    pub num_g2_powers: usize,
+    pub powers_of_tau: PowersOfTau,
+    pub pot_pubkey: Option<String>,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct PowersOfTau {
+    pub g1_powers: Vec<String>,
+    pub g2_powers: Vec<String>,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug, Error)]
+pub enum ContributionsError {
+    #[error("Error in contribution {0}: {1}")]
+    InvalidContribution(usize, #[source] ContributionError),
+    #[error("Unexpected number of contributions: expected {0}, got {1}")]
+    InvalidContributionCount(usize, usize),
+}
+
+#[derive(Clone, Copy, PartialEq, Debug, Error)]
+pub enum ContributionError {
+    #[error("Unexpected number of G1 powers: expected {0}, got {1}")]
+    UnexpectedNumG1Powers(usize, usize),
+    #[error("Unexpected number of G2 powers: expected {0}, got {1}")]
+    UnexpectedNumG2Powers(usize, usize),
+    #[error("Inconsistent number of G1 powers: numG1Powers = {0}, len = {1}")]
+    InconsistentNumG1Powers(usize, usize),
+    #[error("Inconsistent number of G2 powers: numG2Powers = {0}, len = {1}")]
+    InconsistentNumG2Powers(usize, usize),
+    #[error("Error parsing G1 power {0}: {1}")]
+    InvalidG1Power(usize, #[source] ParseError),
+    #[error("Error parsing G2 power {0}: {1}")]
+    InvalidG2Power(usize, #[source] ParseError),
+    #[error("Error parsing potPubkey: {0}")]
+    InvalidPubKey(#[source] ParseError),
+}
+
+impl ContributionsJson {
+    pub fn initial() -> Self {
+        Self {
+            sub_contributions: crate::SIZES
+                .iter()
+                .map(|(num_g1, num_g2)| ContributionJson::initial(*num_g1, *num_g2))
+                .collect(),
+        }
+    }
+
+    pub fn from_json(json: &str) -> Result<Self, ContributionsError> {
+        // let json = serde_json::from_str(json)?;
+        // let validation = schema.validate(&initial);
+        // if !validation.is_strictly_valid() {
+        //     for error in validation.errors {
+        //         error!("{}", error);
+        //     }
+        //     for missing in validation.missing {
+        //         error!("Missing {}", missing);
+        //     }
+        //     // TODO bail!("Initial contribution is not valid.");
+        // }
+        // info!("Initial contribution is json-schema valid.");
+        // TODO:
+        todo!()
+    }
+
+    pub fn parse(&self) -> Result<Vec<Contribution>, ContributionsError> {
+        if self.sub_contributions.len() != crate::SIZES.len() {
+            return Err(ContributionsError::InvalidContributionCount(
+                4,
+                self.sub_contributions.len(),
+            ));
+        }
+        self.sub_contributions
+            .iter()
+            .zip(crate::SIZES.iter())
+            .map(|(c, (num_g1, num_g2))| {
+                if c.num_g1_powers != *num_g1 {
+                    return Err(ContributionError::UnexpectedNumG1Powers(
+                        *num_g1,
+                        c.num_g1_powers,
+                    ));
+                }
+                if c.num_g2_powers != *num_g2 {
+                    return Err(ContributionError::UnexpectedNumG1Powers(
+                        *num_g1,
+                        c.num_g1_powers,
+                    ));
+                }
+                Ok(())
+            })
+            .enumerate()
+            .try_for_each(|(i, result)| {
+                result.map_err(|e| ContributionsError::InvalidContribution(i, e))
+            })?;
+        self.sub_contributions
+            .par_iter()
+            .enumerate()
+            .map(|(i, c)| {
+                c.parse()
+                    .map_err(|e| ContributionsError::InvalidContribution(i, e))
+            })
+            .collect::<Result<Vec<_>, _>>()
+    }
+}
+
+impl ContributionJson {
+    pub fn initial(num_g1_powers: usize, num_g2_powers: usize) -> Self {
+        Self {
+            num_g1_powers,
+            num_g2_powers,
+            powers_of_tau: PowersOfTau::initial(num_g1_powers, num_g2_powers),
+            pot_pubkey: None,
+        }
+    }
+
+    pub fn parse(&self) -> Result<Contribution, ContributionError> {
+        if self.powers_of_tau.g1_powers.len() != self.num_g1_powers {
+            return Err(ContributionError::InconsistentNumG1Powers(
+                self.num_g1_powers,
+                self.powers_of_tau.g1_powers.len(),
+            ));
+        }
+        if self.powers_of_tau.g2_powers.len() != self.num_g2_powers {
+            return Err(ContributionError::InconsistentNumG2Powers(
+                self.num_g2_powers,
+                self.powers_of_tau.g2_powers.len(),
+            ));
+        }
+        let g1_powers = self
+            .powers_of_tau
+            .g1_powers
+            .par_iter()
+            .enumerate()
+            .map(|(i, hex)| {
+                parse_g::<g1::Parameters>(hex).map_err(|e| ContributionError::InvalidG1Power(i, e))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        let g2_powers = self
+            .powers_of_tau
+            .g2_powers
+            .par_iter()
+            .enumerate()
+            .map(|(i, hex)| {
+                parse_g::<g2::Parameters>(hex).map_err(|e| ContributionError::InvalidG2Power(i, e))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        let pubkey = if let Some(pubkey) = &self.pot_pubkey {
+            parse_g::<g2::Parameters>(pubkey).map_err(ContributionError::InvalidPubKey)?
+        } else {
+            G2Affine::zero()
+        };
+        Ok(Contribution {
+            pubkey,
+            g1_powers,
+            g2_powers,
+        })
+    }
+}
+
+impl PowersOfTau {
+    pub fn initial(num_g1_powers: usize, num_g2_powers: usize) -> Self {
+        Self {
+            g1_powers: vec!["0x97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb".to_string(); num_g1_powers],
+            g2_powers: vec!["0x93e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8".to_string(); num_g2_powers],
+        }
+    }
+}
+
+impl Transcript {
+    #[must_use]
+    pub fn new(num_g1: usize, num_g2: usize) -> Self {
+        Self {
+            pubkeys: vec![G2Affine::prime_subgroup_generator()],
+            products: vec![G1Affine::prime_subgroup_generator()],
+            g1_powers: vec![G1Affine::prime_subgroup_generator(); num_g1],
+            g2_powers: vec![G2Affine::prime_subgroup_generator(); num_g2],
+        }
+    }
+}
+
+impl Contribution {
+    pub fn new(num_g1: usize, num_g2: usize) -> Self {
+        Self {
+            pubkey: G2Affine::prime_subgroup_generator(),
+            g1_powers: vec![G1Affine::prime_subgroup_generator(); num_g1],
+            g2_powers: vec![G2Affine::prime_subgroup_generator(); num_g2],
+        }
+    }
+
+    #[instrument(level = "info", skip_all, fields(n1=self.g1_powers.len(), n2=self.g2_powers.len()))]
+    pub fn subgroup_check(&self) {
+        assert!(self.pubkey.is_in_correct_subgroup_assuming_on_curve());
+        self.g1_powers
+            .par_iter()
+            .for_each(|point| assert!(g1_subgroup_check(point)));
+        self.g2_powers
+            .par_iter()
+            .for_each(|point| assert!(g2_subgroup_check(point)));
+    }
+
+    #[instrument(level = "info", skip_all)]
+    pub fn add_tau(&mut self, tau: &Fr) {
+        let n_tau = max(self.g1_powers.len(), self.g2_powers.len());
+        let powers = Self::pow_table(&tau, n_tau);
+        self.mul_g1(&powers[0..self.g1_powers.len()]);
+        self.mul_g2(&powers[0..self.g2_powers.len()]);
+        self.pubkey = self.pubkey.mul(*tau).into_affine();
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn pow_table(tau: &Fr, n: usize) -> Zeroizing<Vec<Fr>> {
+        let mut powers = Zeroizing::new(Vec::with_capacity(n));
+        let mut pow_tau = Zeroizing::new(Fr::one());
+        powers.push(*pow_tau);
+        for _ in 1..n {
+            *pow_tau *= *tau;
+            powers.push(*pow_tau);
+        }
+        powers
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn mul_g1(&mut self, scalars: &[Fr]) {
+        let projective = self
+            .g1_powers
+            .par_iter()
+            .zip(scalars.par_iter())
+            .map(|(c, pow_tau)| g1_mul_glv(c, *pow_tau))
+            .collect::<Vec<_>>();
+        self.g1_powers = G1Projective::batch_normalization_into_affine(&projective[..]);
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn mul_g2(&mut self, scalars: &[Fr]) {
+        let projective = self
+            .g2_powers
+            .par_iter()
+            .zip(scalars.par_iter())
+            .map(|(c, pow_tau)| c.mul(*pow_tau))
+            .collect::<Vec<_>>();
+        self.g2_powers = G2Projective::batch_normalization_into_affine(&projective[..]);
+    }
+
+    #[instrument(level = "info", skip_all)]
+    pub fn verify(&self, transcript: &Transcript) {
+        assert_eq!(self.g1_powers.len(), transcript.g1_powers.len());
+        assert_eq!(self.g2_powers.len(), transcript.g2_powers.len());
+        self.verify_pubkey(transcript.products.last().unwrap());
+        self.verify_g1();
+        self.verify_g2();
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn verify_pubkey(&self, prev_product: &G1Affine) {
+        assert_eq!(
+            Bls12_381::pairing(self.g1_powers[1], G2Affine::prime_subgroup_generator()),
+            Bls12_381::pairing(*prev_product, self.pubkey)
+        );
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn verify_g1(&self) {
+        let (factors, sum) = random_factors(self.g1_powers.len() - 1);
+        let lhs_g1 = VariableBaseMSM::multi_scalar_mul(&self.g1_powers[1..], &factors[..]);
+        let lhs_g2 = G2Affine::prime_subgroup_generator().mul(sum);
+        let rhs_g1 =
+            VariableBaseMSM::multi_scalar_mul(&self.g1_powers[..factors.len()], &factors[..]);
+        let rhs_g2 = self.g2_powers[1].mul(sum);
+        assert_eq!(
+            Bls12_381::pairing(lhs_g1, lhs_g2),
+            Bls12_381::pairing(rhs_g1, rhs_g2)
+        );
+    }
+
+    #[instrument(level = "info", skip_all)]
+    fn verify_g2(&self) {
+        let (factors, sum) = random_factors(self.g2_powers.len());
+        let lhs_g1 =
+            VariableBaseMSM::multi_scalar_mul(&self.g1_powers[..factors.len()], &factors[..]);
+        let lhs_g2 = G2Affine::prime_subgroup_generator().mul(sum);
+        let rhs_g1 = G1Affine::prime_subgroup_generator().mul(sum);
+        let rhs_g2 = VariableBaseMSM::multi_scalar_mul(&self.g2_powers[..], &factors[..]);
+        assert_eq!(
+            Bls12_381::pairing(lhs_g1, lhs_g2),
+            Bls12_381::pairing(rhs_g1, rhs_g2)
+        );
+    }
+}
+
+fn random_factors(n: usize) -> (Vec<<Fr as PrimeField>::BigInt>, Fr) {
+    let mut rng = rand::thread_rng();
+    let mut sum = Fr::zero();
+    let factors = iter::from_fn(|| {
+        let r = Fr::rand(&mut rng);
+        sum += r;
+        Some(r.0)
+    })
+    .take(n)
+    .collect::<Vec<_>>();
+    (factors, sum)
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use ark_ff::UniformRand;
+
+    #[test]
+    fn verify() {
+        let mut transcript = Transcript::new(32768, 65);
+        let mut contrib = Contribution::new(32768, 65);
+        contrib.verify(&transcript);
+        let mut rng = rand::thread_rng();
+        contrib.add_tau(&Fr::rand(&mut rng));
+        contrib.verify(&transcript);
+    }
+}
+
+#[cfg(feature = "bench")]
+#[doc(hidden)]
+pub mod bench {
+    use crate::bench::rand_fr;
+
+    use super::*;
+    use ark_ff::UniformRand;
+    use criterion::{black_box, BatchSize, BenchmarkId, Criterion};
+
+    pub fn group(criterion: &mut Criterion) {
+        bench_pow_tau(criterion);
+        bench_add_tau(criterion);
+        bench_verify(criterion);
+    }
+
+    fn bench_pow_tau(criterion: &mut Criterion) {
+        criterion.bench_function("contribution/pow_tau", move |bencher| {
+            let mut rng = rand::thread_rng();
+            let tau = Zeroizing::new(Fr::rand(&mut rng));
+            bencher.iter(|| black_box(Contribution::pow_table(black_box(&tau), 32768)));
+        });
+    }
+
+    fn bench_add_tau(criterion: &mut Criterion) {
+        for size in crate::SIZES {
+            criterion.bench_with_input(
+                BenchmarkId::new("contribution/add_tau", format!("{:?}", size)),
+                &size,
+                move |bencher, (n1, n2)| {
+                    let mut contrib = Contribution::new(*n1, *n2);
+                    bencher.iter_batched(
+                        || rand_fr(),
+                        |tau| contrib.add_tau(&tau),
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
+    }
+
+    fn bench_verify(criterion: &mut Criterion) {
+        for size in crate::SIZES {
+            criterion.bench_with_input(
+                BenchmarkId::new("contribution/verify", format!("{:?}", size)),
+                &size,
+                move |bencher, (n1, n2)| {
+                    let mut transcript = Transcript::new(*n1, *n2);
+                    let mut contrib = Contribution::new(*n1, *n2);
+                    contrib.add_tau(&rand_fr());
+                    bencher.iter(|| black_box(contrib.verify(&transcript)));
+                },
+            );
+        }
+    }
+}
---
+++ crypto/src/crypto.rs
@@ -0,0 +1,376 @@
+/// Optimized subgroup checks.
+///
+/// Endomorphism and subgroup checks taken from latest (unreleased) arkworks-rs:
+/// See [bls12_381/src/curves/g1.rs](https://github.com/arkworks-rs/curves/blob/dc555882cd867b1e5b6fb16f840ebb0b336136d1/bls12_381/src/curves/g1.rs#L48)
+/// See [bls12_381/src/curves/g2.rs](https://github.com/arkworks-rs/curves/blob/dc555882cd867b1e5b6fb16f840ebb0b336136d1/bls12_381/src/curves/g2.rs#L112)
+use ark_bls12_381::{Fq, Fr, G1Affine, G1Projective, G2Projective, Parameters};
+use ark_bls12_381::{Fq2, G2Affine};
+use ark_ec::{bls12::Bls12Parameters, AffineCurve, ProjectiveCurve};
+use ark_ff::{field_new, Field, PrimeField, Zero};
+use std::ops::{AddAssign, Neg};
+
+/// is_in_correct_subgroup_assuming_on_curve
+#[inline]
+#[must_use]
+pub fn g1_subgroup_check(p: &G1Affine) -> bool {
+    // Algorithm from Section 6 of https://eprint.iacr.org/2021/1130.
+    //
+    // Check that endomorphism_p(P) == -[X^2]P
+
+    // An early-out optimization described in Section 6.
+    // If uP == P but P != point of infinity, then the point is not in the right
+    // subgroup.
+    let x_times_p = g1_mul_bigint(p, Parameters::X);
+    if x_times_p.eq(p) && !p.infinity {
+        return false;
+    }
+
+    let minus_x_squared_times_p = g1_mul_bigint_proj(&x_times_p, Parameters::X).neg();
+    let endomorphism_p = g1_endomorphism(p);
+    minus_x_squared_times_p.eq(&endomorphism_p)
+}
+
+#[inline]
+#[must_use]
+pub fn g2_subgroup_check(point: &G2Affine) -> bool {
+    // Algorithm from Section 4 of https://eprint.iacr.org/2021/1130.
+    //
+    // Checks that [p]P = [X]P
+
+    let mut x_times_point = g2_mul_bigint(point, Parameters::X);
+    if Parameters::X_IS_NEGATIVE {
+        x_times_point = -x_times_point;
+    }
+
+    let p_times_point = g2_endomorphism(point);
+
+    x_times_point.eq(&p_times_point)
+}
+
+#[inline]
+#[must_use]
+fn g1_mul_bigint(base: &G1Affine, scalar: &[u64]) -> G1Projective {
+    let mut res = G1Projective::zero();
+    for b in ark_ff::BitIteratorBE::without_leading_zeros(scalar) {
+        res.double_in_place();
+        if b {
+            res.add_assign_mixed(base);
+        }
+    }
+    res
+}
+
+#[inline]
+#[must_use]
+fn g1_mul_bigint_proj(base: &G1Projective, scalar: &[u64]) -> G1Projective {
+    let mut res = G1Projective::zero();
+    for b in ark_ff::BitIteratorBE::without_leading_zeros(scalar) {
+        res.double_in_place();
+        if b {
+            res += base;
+        }
+    }
+    res
+}
+
+#[inline]
+#[must_use]
+fn g2_mul_bigint(base: &G2Affine, scalar: &[u64]) -> G2Projective {
+    let mut res = G2Projective::zero();
+    for b in ark_ff::BitIteratorBE::without_leading_zeros(scalar) {
+        res.double_in_place();
+        if b {
+            res.add_assign_mixed(base);
+        }
+    }
+    res
+}
+
+#[inline]
+#[must_use]
+pub fn g1_endomorphism(p: &G1Affine) -> G1Affine {
+    /// BETA is a non-trivial cubic root of unity in Fq.
+    const BETA: Fq = field_new!(Fq, "793479390729215512621379701633421447060886740281060493010456487427281649075476305620758731620350");
+
+    // Endomorphism of the points on the curve.
+    // endomorphism_p(x,y) = (BETA * x, y)
+    // where BETA is a non-trivial cubic root of unity in Fq.
+    let mut res = (*p).clone();
+    res.x *= BETA;
+    res
+}
+
+#[inline]
+#[must_use]
+pub fn g2_endomorphism(p: &G2Affine) -> G2Affine {
+    // The p-power endomorphism for G2 is defined as follows:
+    // 1. Note that G2 is defined on curve E': y^2 = x^3 + 4(u+1).
+    //    To map a point (x, y) in E' to (s, t) in E,
+    //    one set s = x / ((u+1) ^ (1/3)), t = y / ((u+1) ^ (1/2)),
+    //    because E: y^2 = x^3 + 4.
+    // 2. Apply the Frobenius endomorphism (s, t) => (s', t'),
+    //    another point on curve E, where s' = s^p, t' = t^p.
+    // 3. Map the point from E back to E'; that is,
+    //    one set x' = s' * ((u+1) ^ (1/3)), y' = t' * ((u+1) ^ (1/2)).
+    //
+    // To sum up, it maps
+    // (x,y) -> (x^p / ((u+1)^((p-1)/3)), y^p / ((u+1)^((p-1)/2)))
+    // as implemented in the code as follows.
+
+    // PSI_X = 1/(u+1)^((p-1)/3)
+    const P_POWER_ENDOMORPHISM_COEFF_0_1: Fq =
+        field_new!(Fq,
+            "4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939437"
+        );
+    // PSI_Y = 1/(u+1)^((p-1)/2)
+    const P_POWER_ENDOMORPHISM_COEFF_1_0: Fq = field_new!(Fq,
+            "2973677408986561043442465346520108879172042883009249989176415018091420807192182638567116318576472649347015917690530");
+    const P_POWER_ENDOMORPHISM_COEFF_1_1: Fq = field_new!(Fq,
+            "1028732146235106349975324479215795277384839936929757896155643118032610843298655225875571310552543014690878354869257");
+
+    let mut res = *p;
+    res.x.frobenius_map(1);
+    res.y.frobenius_map(1);
+
+    let tmp_x = res.x.clone();
+    res.x.c0 = -P_POWER_ENDOMORPHISM_COEFF_0_1 * &tmp_x.c1;
+    res.x.c1 = P_POWER_ENDOMORPHISM_COEFF_0_1 * &tmp_x.c0;
+    res.y *= Fq2::new(
+        P_POWER_ENDOMORPHISM_COEFF_1_0,
+        P_POWER_ENDOMORPHISM_COEFF_1_1,
+    );
+
+    res
+}
+
+const G1_LAMBDA: u64 = 0xd201_0000_0001_0000;
+const G1_LAMBDA_2: [u64; 2] = [0x0000_0001_0000_0000, 0xac45_a401_0001_a402];
+
+#[inline]
+#[must_use]
+fn g1_split(tau: Fr) -> (u128, u128) {
+    let mut tau = tau.into_repr().0;
+    let mut divisor = G1_LAMBDA_2;
+    ruint::algorithms::div_rem(&mut tau, &mut divisor);
+    let k0 = (divisor[0] as u128) | (divisor[1] as u128) << 64;
+    let k1 = (tau[0] as u128) | (tau[1] as u128) << 64;
+    (k0, k1)
+}
+
+/// Implements scalar-point multiplication using Gallant-Lambert-Vanstone (GLV).
+#[must_use]
+pub fn g1_mul_glv(p: &G1Affine, tau: Fr) -> G1Projective {
+    let (k0, k1) = g1_split(tau);
+
+    // Find first bit set
+    if k0 | k1 == 0 {
+        return G1Projective::zero();
+    }
+    let mut bit = 1_u128 << (127 - (k0 | k1).leading_zeros());
+
+    // Compute endomorphism
+    let q = g1_endomorphism(p).neg();
+    let pq = p.into_projective().add_mixed(&q);
+
+    let mut res = G1Projective::zero();
+    loop {
+        let b0 = bit & k0 != 0;
+        let b1 = bit & k1 != 0;
+        match (b0, b1) {
+            (true, true) => res.add_assign(&pq),
+            (true, false) => res.add_assign_mixed(p),
+            (false, true) => res.add_assign_mixed(&q),
+            (false, false) => {}
+        }
+        bit >>= 1;
+        if bit == 0 {
+            break;
+        }
+        res.double_in_place();
+    }
+    res
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use crate::test::{arb_fr, arb_g1, arb_g2};
+    use ark_ec::AffineCurve;
+    use ark_ff::{BigInteger256, PrimeField};
+    use proptest::proptest;
+
+    #[test]
+    fn test_g1_endomorphism() {
+        proptest!(|(p in arb_g1())| {
+            let expected = g1_mul_bigint(&p, &G1_LAMBDA_2).neg().into_affine();
+            let value = g1_endomorphism(&p);
+            assert_eq!(value, expected);
+        });
+    }
+
+    #[test]
+    fn test_g1_check() {
+        // TODO: Generate points not in prime order subgroup.
+        proptest!(|(p in arb_g1())| {
+            let expected = p.is_in_correct_subgroup_assuming_on_curve();
+            let value = g1_subgroup_check(&p);
+            assert_eq!(value, expected);
+        });
+    }
+
+    #[test]
+    fn test_g1_split() {
+        let lambda = Fr::from_repr(BigInteger256([G1_LAMBDA_2[0], G1_LAMBDA_2[1], 0, 0])).unwrap();
+        proptest!(|(s in arb_fr())| {
+            let (k0, k1) = g1_split(s);
+            let value = Fr::from(k0) + Fr::from(k1) * lambda;
+            assert_eq!(value, s);
+        });
+    }
+
+    #[test]
+    fn test_g1_mul_glv() {
+        proptest!(|(p in arb_g1(), s in arb_fr())| {
+            let expected = p.mul(s);
+            let value = g1_mul_glv(&p, s);
+            assert_eq!(value, expected);
+        });
+    }
+
+    #[test]
+    fn test_g2_endomorphism() {
+        proptest!(|(p in arb_g2())| {
+            let value = g2_endomorphism(&p);
+            // TODO: Compute expected value
+            // let expected = g2_mul_bigint(&p, &G1_LAMBDA_2).neg().into_affine();
+            // assert_eq!(value, expected);
+        });
+    }
+
+    #[test]
+    fn test_g2_check() {
+        // TODO: Generate points not in prime order subgroup.
+        proptest!(|(p in arb_g2())| {
+            let value = g2_subgroup_check(&p);
+            let expected = p.is_in_correct_subgroup_assuming_on_curve();
+            assert_eq!(value, expected);
+        });
+    }
+}
+
+#[cfg(feature = "bench")]
+#[doc(hidden)]
+pub mod bench {
+    use super::*;
+    use crate::bench::{rand_fr, rand_g1, rand_g2};
+    use criterion::{black_box, BatchSize, Criterion};
+
+    pub fn group(criterion: &mut Criterion) {
+        bench_g1_endo(criterion);
+        bench_g1_check(criterion);
+        bench_g1_check_endo(criterion);
+        bench_g1_mul(criterion);
+        bench_g1_split(criterion);
+        bench_g1_mul_glv(criterion);
+        bench_g2_endo(criterion);
+        bench_g2_check(criterion);
+        bench_g2_check_endo(criterion);
+        bench_g2_mul(criterion);
+    }
+
+    fn bench_g1_endo(criterion: &mut Criterion) {
+        criterion.bench_function("g1_endomorphism", move |bencher| {
+            bencher.iter_batched(
+                rand_g1,
+                |p| black_box(g1_endomorphism(&p)),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g1_check(criterion: &mut Criterion) {
+        criterion.bench_function("g1_subgroup_check", move |bencher| {
+            bencher.iter_batched(
+                rand_g1,
+                |p| black_box(p.is_in_correct_subgroup_assuming_on_curve()),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g1_check_endo(criterion: &mut Criterion) {
+        criterion.bench_function("g1_subgroup_check_endo", move |bencher| {
+            bencher.iter_batched(
+                rand_g1,
+                |p| black_box(g1_subgroup_check(&p)),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g1_mul(criterion: &mut Criterion) {
+        criterion.bench_function("g1_mul", move |bencher| {
+            bencher.iter_batched(
+                || (rand_g1(), rand_fr()),
+                |(p, s)| black_box(p.mul(black_box(s))),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g1_split(criterion: &mut Criterion) {
+        criterion.bench_function("g1_split", move |bencher| {
+            bencher.iter_batched(rand_fr, |s| black_box(g1_split(s)), BatchSize::SmallInput);
+        });
+    }
+
+    fn bench_g1_mul_glv(criterion: &mut Criterion) {
+        criterion.bench_function("g1_mul_glv", move |bencher| {
+            bencher.iter_batched(
+                || (rand_g1(), rand_fr()),
+                |(p, s)| black_box(g1_mul_glv(black_box(&p), black_box(s))),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g2_endo(criterion: &mut Criterion) {
+        criterion.bench_function("g2_endomorphism", move |bencher| {
+            bencher.iter_batched(
+                rand_g2,
+                |p| black_box(g2_endomorphism(&p)),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g2_check(criterion: &mut Criterion) {
+        criterion.bench_function("g2_subgroup_check", move |bencher| {
+            bencher.iter_batched(
+                rand_g2,
+                |p| black_box(p.is_in_correct_subgroup_assuming_on_curve()),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g2_check_endo(criterion: &mut Criterion) {
+        criterion.bench_function("g2_subgroup_check_endo", move |bencher| {
+            bencher.iter_batched(
+                rand_g2,
+                |p| black_box(g2_subgroup_check(&p)),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    fn bench_g2_mul(criterion: &mut Criterion) {
+        criterion.bench_function("g2_mul", move |bencher| {
+            bencher.iter_batched(
+                || (rand_g2(), rand_fr()),
+                |(p, s)| black_box(p.mul(black_box(s))),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+}
