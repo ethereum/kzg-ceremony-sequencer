be340fddf9dca50b16bd5ee6145a10faad3e35a4
Remco Bloemen <remco@wicked.ventures>
2022-10-10 08:22:45 -0500

    Eip712 wip

--- crypto/src/signature/mod.rs
+++ crypto/src/signature/mod.rs
@@ -1,6 +1,11 @@
+//! Implements the binding the contribution to participants.
+//! https://github.com/ethereum/kzg-ceremony-specs/blob/master/docs/cryptography/contributionSigning.md
+//! https://github.com/gakonst/ethers-rs/blob/e89c7a378bba6587e3f525982785c59a33c14d9b/ethers-core/ethers-derive-eip712/tests/derive_eip712.rs
+
 mod identity;
 
 use crate::G1;
+use ethers;
 
 pub enum BlsSignature {
     None,
@@ -12,4 +17,19 @@ pub enum EcdsaSignature {
     None,
     G1(G1),
     String(String),
+}
+
+
+#[derive(Debug, Clone, Eip712, EthAbiType)]
+#[eip712(
+    name = "Radicle",
+    version = "1",
+    chain_id = 1,
+    verifying_contract = "0x0000000000000000000000000000000000000001"
+)]
+pub struct Puzzle {
+    pub organization: H160,
+    pub contributor: H160,
+    pub commit: String,
+    pub project: String,
 }
